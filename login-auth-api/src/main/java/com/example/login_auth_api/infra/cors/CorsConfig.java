f8dc84df5f3011141942ab1d8ecd2d3fcac204e5
user <leticiacastrojacobmarques@gmail.com>
2026-06-05 18:52:30 -0300

    Feat: Cors config

--- login-auth-api/src/main/java/com/example/login_auth_api/infra/cors/CorsConfig.java
+++ login-auth-api/src/main/java/com/example/login_auth_api/infra/cors/CorsConfig.java
@@ -9,7 +9,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
-                .allowedMethods("GET", "POST", "DELETE", "PUT").allowedHeaders("*");
+                .allowedOrigins("http://localhost:4200", "https://gestao-de-projetos.netlify.app")
+                .allowedMethods("GET", "POST", "DELETE", "PUT")
+                .allowedHeaders("*");
     }
 }
